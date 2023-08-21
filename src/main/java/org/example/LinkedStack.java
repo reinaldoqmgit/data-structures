6f16db7e734716112e378fc4710dc2e96f9f5cdc
reinaldo <quispe.medrano23@gmail.com>
2023-08-21 16:15:38 -0400

    Implementation of LinkedStack class

---
+++ src/main/java/org/example/LinkedStack.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class LinkedStack<E> implements Stack<E> {
+    private SinglyLinkedList<E> singlyLinkedList = new SinglyLinkedList<>();
+    @Override
+    public int size() {
+        return this.singlyLinkedList.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.singlyLinkedList.isEmpty();
+    }
+
+    @Override
+    public void push(E element) {
+        this.singlyLinkedList.addFirst(element);
+    }
+
+    @Override
+    public E top() {
+        return this.singlyLinkedList.getFirst();
+    }
+
+    @Override
+    public E pop() {
+        return this.singlyLinkedList.removeFirst();
+    }
+}
